4075d52613924905e11999e338e1f70bc1e6b877
DVampire <1271421361@qq.com>
2025-03-17 14:06:49 +0800

    init data

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1005,6 +1005,7 @@ class RayPPOTrainer(object):
                     # recompute old_log_probs
                     with _timer('old_log_prob', timing_raw):
                         old_log_prob = self.actor_rollout_wg.compute_log_prob(batch)
+                        print('old_log_prob', old_log_prob.batch.keys())
                         batch = batch.union(old_log_prob)
 
                     if self.use_reference_policy:
